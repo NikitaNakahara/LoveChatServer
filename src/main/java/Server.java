e41f43f43bebbd53dae8dcc5da5f796fe7c237f8
NikitaNakahara <nikita.maer14@gmail.com>
2022-10-05 22:37:05 +0500

    added system commands

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -9,13 +9,50 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Server {
-    public void start() throws IOException {
+    boolean stopServer = false;
 
+    public void start() throws IOException {
         try (ServerSocket server = new ServerSocket(8080)) {
             System.out.println("Start server");
-            //noinspection InfiniteLoopStatement
-            while (true) {
+
+            Scanner scanner = new Scanner(System.in);
+            new Thread(() -> {
+                while (true) {
+                    switch (scanner.nextLine()) {
+                        case "stop":
+                            if (!stopServer) {
+                                stopServer = true;
+                            } else {
+                                System.out.println("server already stopped\n");
+                            }
+                            break;
+
+                        case "start":
+                            if (stopServer) {
+                                stopServer = false;
+                                new Thread(() -> startMainLoop(server));
+                            } else {
+                                System.out.println("server already started\n");
+                            }
+                            break;
+
+                        default:
+                            System.out.println("unexpected command\n");
+                            break;
+                    }
+                }
+            }).start();
+
+            startMainLoop(server);
+        }
+    }
+
+    private void startMainLoop(ServerSocket server) {
+        while (!stopServer) {
+            try {
                 newClient(server.accept());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
