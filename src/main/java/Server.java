4098a0ce620ec59a82a2d2363a1af9748271222f
NikitaNakahara <nikita.maer14@gmail.com>
2022-10-08 14:31:53 +0500

    added users map and users id

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -9,6 +9,8 @@ import java.util.*;
 public class Server {
     boolean stopServer = false;
 
+    Map<String, DataOutputStream> usersMap = new HashMap<>();
+
     public void start() throws IOException {
         try (ServerSocket server = new ServerSocket(8080)) {
             System.out.println("Start server");
@@ -67,11 +69,41 @@ public class Server {
                     String message = input.readUTF();
                     System.out.println(message);
                     JSONObject json = new JSONObject(message);
-                    if (json.getString("type").equals("msg")) {
-                        Map<String, String> map = new HashMap<>();
-                        map.put("type", "msg");
-                        map.put("text", json.getString("text"));
-                        output.writeUTF(new JSONObject(map).toString());
+                    switch (json.getString("type")) {
+                        case "msg": {
+                            Map<String, String> map = new HashMap<>();
+                            map.put("type", "msg");
+                            map.put("text", json.getString("text"));
+                            DataOutputStream dos = findUserInOnline(json.getString("id"));
+                            if (dos != null) {
+                                dos.writeUTF(new JSONObject(map).toString());
+                            } else {
+                                System.out.println("user not found");
+                            }
+                            break;
+                        }
+
+                        case "sys": {
+                            switch (json.getString("text")) {
+                                case "get_id": {
+                                    String id = genUniqueId();
+                                    Map<String, String> map = new HashMap<>();
+                                    map.put("type", "sys");
+                                    map.put("text", "user_id");
+                                    map.put("id", id);
+                                    output.writeUTF(new JSONObject(map).toString());
+                                    break;
+                                }
+
+                                case "online": {
+                                    String id = json.getString("id");
+                                    addUserInOnline(output, id);
+                                    break;
+                                }
+                            }
+
+                            break;
+                        }
                     }
                 } catch (IOException e) {
                     System.out.println("Client is disconnected");
@@ -79,5 +111,27 @@ public class Server {
                 }
             }
         }).start();
+    }
+
+
+
+    private String genUniqueId() {
+        StringBuilder id = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            int randomInt = random.nextInt(9);
+            id.append(randomInt);
+        }
+
+        return id.toString();
+    }
+
+    private void addUserInOnline(DataOutputStream output, String id) {
+        usersMap.put(id, output);
+    }
+
+    private DataOutputStream findUserInOnline(String id) {
+        return usersMap.get(id);
     }
 }
